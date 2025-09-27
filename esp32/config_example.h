c7168fa5d1c7f1a5af78f6cffac6ef23e3e1c349
Shchurov Nickolay <shchurov_no2@mail.ru>
2025-09-27 18:36:28 +0300

    add code to be uploaded on esp32

---
+++ esp32/config_example.h
@@ -0,0 +1,4 @@
+// rename this file to "config.h"
+const char* ssid = "your_wifi_ssid";
+const char* password = "your_wifi_password";
+const char* serverURL = "http://192.168.1.186:8000/sensor";
---
+++ esp32/esp32.ino
@@ -0,0 +1,73 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+Adafruit_BME280 bme; // I2C
+
+unsigned long delayTime;
+
+void setup() {
+    Serial.begin(9600);
+    while(!Serial);
+    Serial.println(F("BME280 test"));
+
+    unsigned status;
+    status = bme.begin(0x76); // 0x76 is the default address of esp32
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+        while (1) delay(10);
+    }
+    
+    Serial.println("-- Default Test --");
+    delayTime = 6000;
+
+    Serial.println();
+
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+  Serial.println(" Connected!");
+}
+
+void loop() { 
+    printValues();
+    delay(delayTime);
+}
+
+void printValues() {
+    float temp = bme.readTemperature();
+    float hum = bme.readHumidity();
+    float pres = bme.readPressure() / 100.0F;
+
+    // Create JSON payload
+    StaticJsonDocument<200> doc;
+    doc["temp"] = temp;
+    doc["hum"] = hum;
+    doc["pres"] = pres;
+    String jsonPayload;
+    serializeJson(doc, jsonPayload);
+
+    HTTPClient http;
+    http.begin(serverURL);
+    http.addHeader("Content-Type", "application/json");
+    int httpCode = http.POST(jsonPayload);
+    String response = http.getString();
+    http.end();
+
+    Serial.printf("Response: %d, %s\n", httpCode, response.c_str());
+  }
